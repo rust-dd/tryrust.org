90d4ebe2e57d7605a6dd336504486f216537eba0
zeldan <zelei.dan@gmail.com>
2024-07-31 21:01:47 +0200

    feat: remove session folders older than 3 days

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
 #[tokio::main]
 async fn main() {
     use axum::Router;
+    use chrono::{Duration, Utc};
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
+    use std::fs;
     use tryrust::app::*;
     use tryrust::fileserv::file_and_error_handler;
     use tryrust::redirect::redirect_www;
+    use tokio_cron_scheduler::{Job, JobScheduler};
     use tower_http::trace::TraceLayer;
 
     tracing_subscriber::fmt()
@@ -15,11 +18,26 @@ async fn main() {
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    // Setting get_configuration(None) means we'll be using cargo-leptos's env values
-    // For deployment these variables are:
-    // <https://github.com/leptos-rs/start-axum#executing-a-server-on-a-remote-machine-without-the-toolchain>
-    // Alternately a file can be specified such as Some("Cargo.toml")
-    // The file would need to be included with the executable when moved to deployment
+    let scheduler = JobScheduler::new().await.unwrap();
+    let _ = scheduler.add(
+        Job::new_async("0 * * * * *", |_uuid, _l| {
+            Box::pin(async move {
+                let paths = fs::read_dir("./sessions").unwrap();
+                for path in paths {
+                    let full_folder_path = path.unwrap().path();
+                    let folder_last_part = full_folder_path.components().last().unwrap().as_os_str().to_str().unwrap();
+                    let session_creation_time = folder_last_part.parse::<i64>().unwrap();
+                    let three_days_old = (Utc::now() - Duration::days(3)).timestamp_millis();
+                    if session_creation_time < three_days_old {
+                        let _ = fs::remove_dir_all(full_folder_path);
+                    }
+                }
+                tracing::info!("Cleaning folders older than 3 days...");
+            })
+        }).unwrap()
+    ).await;
+    let _ = scheduler.start().await;
+
     let conf = get_configuration(None).await.unwrap();
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
